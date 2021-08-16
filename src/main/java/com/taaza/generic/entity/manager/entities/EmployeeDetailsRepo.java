6da8940b9e80ac8b4d4af732733a4cb6382bd4d9
akhil sood <akhil.sood72@gmail.com>
2021-08-16 21:54:20 +0100

    entity manager with tests

--- src/main/java/com/taaza/generic/entity/manager/entities/EmployeeDetailsRepo.java
+++
@@ -1,8 +0,0 @@
-package com.taaza.generic.entity.manager.entities;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeDetailsRepo extends JpaRepository<EmployeeDetails, Long> {
-}
